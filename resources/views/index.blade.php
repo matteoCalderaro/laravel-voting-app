c5cddeb7128b6e71cf47f5828c4497c33b03695b
Matteo Calderaro <matteocalderaro@gmail.com>
2022-02-27 12:01:51 +0100

    Episode 5 - Design single idea page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,16 +38,18 @@
                     uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Vote</button>
                 </div>
             </div>
-            <div class="flex px-2 py-6">
-                <a href="#" class="flex-none">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 ht-14 rounded-xl">
-                </a>
-                <div class="mx-4">
+            <div class="flex flex-1 px-2 py-6">
+                <div class="flex-none">
+                    <a href="#">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 ht-14 rounded-xl">
+                    </a>
+                </div>
+                <div class="mx-4 w-full">
                     <h4 class="text-xl font-semibold">
                         <a href="#" class="hover:underline">A random title can go here</a>
                     </h4>
                     <div class="text-gray-600 mt-3 line-clamp-3">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, odit expedita, iusto ratione, voluptates nostrum similique iure repellat id officiis voluptatem asperiores quidem vel cum modi exercitationem! Eveniet nam facilis et ut ratione rem porro. Esse dolore quaerat ex ipsa mollitia debitis tenetur earum explicabo rerum saepe eos, quisquam asperiores. Corporis ducimus voluptatibus suscipit ab error, alias voluptate vitae distinctio consequuntur eaque pariatur ipsam earum odio nobis nulla fuga dignissimos similique maiores et ad cupiditate veritatis. Odit eveniet repudiandae saepe, impedit sint iste necessitatibus vero perspiciatis unde omnis, cum quasi, magni fugit. Ut quo labore architecto est, odio ipsam eligendi!
+                        Lorem ipsum, dolor sit amet
                     </div>
                     <div class="flex items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
@@ -60,7 +62,7 @@
                         <div class="flex items-center space-x-2">
                             <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full
                             text-center w-28 h-7 py-2 px-4">Open</div>
-                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3 border">
                                 <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
                                 <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
                                     <li><a href="#" class="hover:bg-gray-100 transition
