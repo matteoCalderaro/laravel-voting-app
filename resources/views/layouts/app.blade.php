35e45bbacd1ef435bd46303d9eec530d5f06aa15
Matteo Calderaro <matteocalderaro@gmail.com>
2022-03-02 10:09:06 +0100

    Episode 15 - Create Ideas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,41 +64,35 @@
                 >
                     <div class="text-center px-6 py-2 pt-6">
                         <h3 class="font-semibold text-base">Add an idea</h3>
-                        <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+                        <p class="text-xs mt-4">
+                            @auth
+                                Let us know what you would like and we'll take a look over!
+                            @else
+                                Please login to create an idea.
+                            @endauth
+                        </p>
                     </div>
-                    <form action="#" method="post" class="space-y-4 px-4 py-6">
-                        <input type="text" class="w-full text-sm bg-gray-100 rounded-xl border-none placeholder-gray-900 px-4 py-2"
-                        placeholder="Your Idea">
-                        <div>
-                            <select name="category_add" id="category_add" class="w-full text-sm rounded-xl border-none bg-gray-100 px-4 py-2">
-                                <option value="Category One">Category One</option>
-                                <option value="Category Two">Category Two</option>
-                                <option value="Category Three">Category Three</option>
-                                <option value="Category Four">Category Four</option>
-                            </select>
-                        </div>
-                        <textarea name="idea" id="idea" cols="30" rows="4" class="w-full border-none bg-gray-100 rounded-xl placeholder-gray-900
-                        text-sm px-4 py-2" placeholder="Describe your idea"></textarea>
-                        <div class="flex items-center justify-between space-x-3">
-                            <button
-                                type="button"
-                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border
-                                border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3"
-                                >
-                                <svg class="text-gray-600 w-4 -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
-                                  </svg>
-                                <span class="ml-1">Attach</span>
-                            </button>
-                            <button
-                                type="submit"
-                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border
+                    @auth
+                        <livewire:create-idea />
+                    @else
+                        <div class="my-5 text-center">
+                            <a
+                                href="{{ route('login')}}"
+                                class="inline-block justify-center w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border
                                 border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3 text-white"
-                                >
-                                <span class="ml-1">Submit</span>
-                            </button>
+                            >
+                                Login
+                            </a>
+                            <a
+                                href="{{ route('register')}}"
+                                class="inline-block justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border
+                                border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3 mt-4"
+                            >
+                                Sign Up
+                            </a>
                         </div>
-                    </form>
+                    @endauth
+
                 </div>
             </div>
             <div class="w-full md:w-175 px-2 md:px-0">
