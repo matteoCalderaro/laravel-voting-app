051af4ad1eced5852710f5d8634ba1631b465bee
Matteo Calderaro <matteocalderaro@gmail.com>
2022-03-08 09:33:26 +0100

    Episode 29 - Other Filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -16,11 +16,13 @@ class IdeasIndex extends Component
 
     public $status;
     public $category;
+    public $filter;
 
 
     protected $queryString = [
         'status',
-        'category'
+        'category',
+        'filter'
     ];
 
     protected $listeners = ['queryStringUpdatedStatus'];
@@ -29,10 +31,19 @@ class IdeasIndex extends Component
         $this->status = request()->status ?? 'All';
     }
 
-
-
     public function updatingCategory(){
         $this->resetPage();
+    }
+
+    public function updatingFilter(){
+        $this->resetPage();
+    }
+    public function updatedFilter(){
+        if ($this->filter == 'My Ideas'){
+            if (! auth()->check()){
+                return redirect()->route('login');
+            }
+        }
     }
 
     public function queryStringUpdatedStatus($newStatus){
@@ -54,6 +65,12 @@ class IdeasIndex extends Component
                 })
                 ->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
                     return $query->where('category_id', $categories->pluck('id','name')->get($this->category));
+                })
+                ->when($this->filter && $this->filter == 'Top Voted', function ($query) {
+                    return $query->orderByDesc('votes_count');
+                })
+                ->when($this->filter && $this->filter == 'My Ideas', function ($query) {
+                    return $query->where('user_id', auth()->id());
                 })
                 ->addSelect(['voted_by_user'=> Vote::select('id')
                     ->where('user_id', auth()->id())
