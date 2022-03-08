d37fcacf8d92b31d8ed5282e266621b16bda7411
Matteo Calderaro <matteocalderaro@gmail.com>
2022-03-08 18:03:46 +0100

    Episode 34 - Set Status Notify All Voters

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire;
 use App\Models\Idea;
 use Livewire\Component;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\IdeaStatusUpdatedMailable;
 
 
 
@@ -13,6 +15,7 @@ class SetStatus extends Component
 
     public $idea;
     public $status;
+    public $notifyAllVoters;
 
     public function mount(Idea $idea){
         $this->idea = $idea;
@@ -24,15 +27,28 @@ class SetStatus extends Component
         if(! auth()->check() || ! auth()->user()->isAdmin()){
             abort(Response::HTTP_FORBIDDEN);
         }
-        else {
 
-            $this->idea->status_id = $this->status;
-            $this->idea->save();
+        $this->idea->status_id = $this->status;
+        $this->idea->save();
 
-            $this->emit('statusWasUpdated');
+        if($this->notifyAllVoters){
+            $this->notifyAllVoters();
         }
 
+        $this->emit('statusWasUpdated');
     }
+
+    public function notifyAllVoters(){
+        $this->idea->votes()
+            ->select('name','email')
+            ->chunk(100, function($voters){
+                foreach ($voters as $user){
+                    Mail::to($user)
+                        ->queue(new IdeaStatusUpdatedMailable($this->idea));
+                }
+            });
+    }
+
 
     public function render()
     {
---
+++ app/Mail/IdeaStatusUpdatedMailable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Idea;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class IdeaStatusUpdatedMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $idea;
+
+    public function __construct(Idea $idea)
+    {
+        $this->idea = $idea;
+    }
+
+
+    public function build()
+    {
+        return $this->subject('An idea you voted for has a new status')
+            ->markdown('emails.idea-status-updated');
+    }
+}
