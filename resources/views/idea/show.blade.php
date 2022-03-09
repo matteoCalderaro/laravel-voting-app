b3c61026649e7c71ad8eb2f0a3cd37aede92a632
Matteo Calderaro <matteocalderaro@gmail.com>
2022-03-09 18:23:59 +0100

    Episode 39 -Edit Idea with Policies

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -11,8 +11,10 @@
         :idea="$idea"
         :votesCount="$votesCount"
     />
-    <livewire:edit-idea />
 
+    @can('update',$idea)
+        <livewire:edit-idea :idea="$idea" />
+    @endcan
 
     <div class="comments-container relative space-y-6 md:ml-22 my-8 pt-4 mt-1">
         @foreach (range(1,3) as $comment)
